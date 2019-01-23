e93016e5cb064a75c2de3cb084e57966680913ee
PhungCongDat <phungdat.nd@gmail.com>
2019-01-23 11:03:33 +0700

    5th commit

--- restaurant/js/navigation.js
+++ restaurant/js/navigation.js
@@ -4,63 +4,20 @@
  */
 
 ( function( $ ) {
-    var pull        = $('#pull'),
-        menu        = $('.menu-menu-1-container');
+    var pull   = $('#pull'),
+        toggle = $( '#toggle' ),
+        menu   = $('.menu-menu-1-container');
  
     pull.on('click', function( e ) {
         e.preventDefault();
         menu.slideToggle();
+        toggle.toggleClass( 'on' );
     });
 
-    $(window).resize(function(){
-        var w = $(window).width();
-        if(w > 991 && menu.is(':hidden')) {
+    $( window ).resize( function() {
+        var w = $( window ).width();
+        if( w > 991 && menu.is( ':hidden' ) ) {
             menu.removeAttr('style');
         }
     });
 })( jQuery );
-
-
-// pull button.
-var theToggle = document.getElementById('toggle');
-
-// based on Todd Motto functions
-// https://toddmotto.com/labs/reusable-js/
-
-// hasClass
-function hasClass(elem, className) {
-    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-        elem.className += ' ' + className;
-    }
-}
-// removeClass
-function removeClass(elem, className) {
-    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
-    }
-}
-
-theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   return false;
-}
